99746c0acf361eade54ac447889e8f3757bcc9ab
Jony <jony@jonylim.com>
2019-12-16 12:01:59 +0700

    initial code

---
+++ js-message-queue.js
@@ -0,0 +1,34 @@
+class JSMessageQueue {
+    constructor() {
+        this.queue = [];
+        this.isRunning = false;
+        this.callback = null;
+    }
+    
+    setCallback(callback) {
+        if (typeof(callback) != "function") {
+            throw new Error("callback must be function");
+        }
+        this.callback = callback;
+    }
+    
+    addToQueue(item) {
+        this.queue.push(item);
+        if (!this.isRunning) {
+            this.isRunning = true;
+            this.processNext();
+        }
+    }
+    
+    processNext(stopIfEmpty) {
+        if (this.queue.length != 0) {
+            this.callback(this.queue.shift());
+        } else if (!stopIfEmpty) {
+            setTimeout(() => {
+                this.processNext();
+            }, 100);
+        } else {
+            this.isRunning = false;
+        }
+    }
+}
